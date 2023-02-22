8da2ac20fd34776bf6db106e6fd2c38452b8f2ea
capnemo20 <z.sujon20@outlook.com>
2023-02-22 13:12:12 +0000

    45-7 (advanced) State, component state hook and set state method

--- react-core-concepts-2/src/App.js
+++ react-core-concepts-2/src/App.js
@@ -1,24 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
+ 
+  return (
+    <div className="App">
+      <Counter></Counter>
+    </div>
+  );
+}
+
+function Counter(){
+  const [count, setCount] = useState(55);
+  const increaseCount = ()=>setCount(count+1);
+  const decreaseCount =()=>setCount(count-1);
+    
+
+  // const increaseCount = ()=>{
+  //   const newCount = count+1;
+  //   setCount(newCount);
+  // }
+  
+  return (
+    <div>
+      <h1>Count: {count} </h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+// <<<<------------------------------------------------------------>>>>
+/*
+ const products = [
     {name:"laptop", price:46000},
     {name:"phone", price:93000},
     {name:"watch", price:7000},
     {name:"tablet", price:50000}
   ]
-  return (
-    <div className="App">
-      {
+*/
+
+/*
+{
         products.map(product=><Product name={product.name} price={product.price}></Product>)
       }
       {/* <Product name="laptop" price="46000"></Product>
       <Product name="phone" price="75000"></Product>
-      <Product name="watch" price="5000"></Product> */}
-    </div>
-  );
-}
+      <Product name="watch" price="5000"></Product> }
+*/
+// <<<<------------------------------------------------------------>>>>
 
 function Product(props){
   return (
