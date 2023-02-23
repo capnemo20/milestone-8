a3f5f2ee3b05aa3372998102bedfe5452f4f267d
capnemo20 <z.sujon20@outlook.com>
2023-02-23 00:18:59 +0000

    46-1 Module Introduction, load countries using useEffect

---
+++ react-rest-countries/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+     <LoadCountries></LoadCountries>
+    </div>
+  );
+}
+
+function LoadCountries(){
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res=> res.json())
+    .then(data=>setCountries(data))
+  }, [])
+  return(
+    <div>
+      <h1>Visiting Every Country of the world</h1>
+      {/* {
+        countries.map(country=> <LoadCountries ></LoadCountries>)
+      } */}
+      <h3>Available countries: {countries.length}</h3>
+    </div>
+  )
+}
+
+export default App;
